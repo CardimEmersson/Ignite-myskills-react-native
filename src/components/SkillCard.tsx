26e8ba69ddc8d65b5cff9873c07a60824bf51188
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2021-08-21 22:53:33 -0300

    add typescript

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  StyleSheet,
+} from "react-native";
 
-export function SkillCard({ skill }) {
+interface SkillCardProps extends TouchableOpacityProps {
+  skill: string;
+}
+
+export function SkillCard({ skill, ...rest }: SkillCardProps) {
   return (
-    <TouchableOpacity style={styles.buttonSkill}>
+    <TouchableOpacity style={styles.buttonSkill} {...rest}>
       <Text style={styles.textSkill}>{skill}</Text>
     </TouchableOpacity>
   );
 }
+
 const styles = StyleSheet.create({
   buttonSkill: {
     backgroundColor: "#1f1e25",
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,26 @@ import {
 import { Button } from "../components/Button";
 import { SkillCard } from "../components/SkillCard";
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState("");
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greeting, setGreeting] = useState("");
 
   function handleNewAddSkill() {
-    setMySkills([...mySkills, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+    setMySkills([...mySkills, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
   }
 
   useEffect(() => {
@@ -42,13 +55,18 @@ export function Home() {
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-      <Button onPress={handleNewAddSkill} />
+      <Button onPress={handleNewAddSkill} title="Add" />
       <Text style={[styles.title, { marginVertical: 50 }]}>My skills</Text>
 
       <FlatList
         data={mySkills}
-        keyExtractor={(item, index) => index}
-        renderItem={({ item }) => <SkillCard skill={item} />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
       />
     </View>
   );
